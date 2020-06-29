4c3849837dc3219f02dbdeba2773d548f0c83b86
Tim Roberts <ten3roberts@gmail.com>
2020-06-29 17:41:54 +0200

    pub use vectors

--- sprocket/src/lib.rs
+++ sprocket/src/lib.rs
@@ -2,3 +2,4 @@ pub mod application;
 pub mod logger;
 pub mod math;
 pub use application::Application;
+pub use math::{Vec2, Vec3, Vec4};
--- sprocket/src/math/mod.rs
+++ sprocket/src/math/mod.rs
@@ -1,2 +1,7 @@
-pub mod vector;
-pub use vector::Vec3;
+pub mod vec2;
+pub mod vec3;
+pub mod vec4;
+
+pub use vec2::Vec2;
+pub use vec3::Vec3;
+pub use vec4::Vec4;
